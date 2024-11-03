51d5f3719d65afa4c1ec0281d187c535bd171ce1
KDlove2k <19120441@student.hcmus.edu.vn>
2024-11-03 07:06:22 +0700

    Socoban

--- dfs.py
+++ dfs.py
@@ -1,9 +1,17 @@
 from support_function import*
-
+import time
+'''
+//========================//
+//          DFS           //
+//        ALGORITHM       //
+//     IMPLEMENTATION     //
+//========================//
+'''
 def DFS_search(initial_board, list_check_point):
     """Perform Depth-First Search to find the solution."""
+    start_time = time.time()
+    list_state = [start_state]
     stack = []
-    
     start_state = state(initial_board, None, list_check_point)
     stack.append(start_state)
     
@@ -25,16 +33,51 @@ def DFS_search(initial_board, list_check_point):
         
         # Find the player's current position
         cur_pos = find_position_player(current_state.board)
-        
+        ''' 
+        THIS WILL PRINT THE STEP-BY-STEP IMPLEMENTATION OF HOW THE ALGORITHM WORKS, 
+        UNCOMMENT TO USE IF NECCESSARY 
+        '''
+        '''
+        time.sleep(1)
+        clear = lambda: os.system('cls')
+        clear()
+        print_matrix(now_state.board)
+        print("State visited : {}".format(len(list_state)))
+        print("State in queue : {}".format(len(list_visit)))
+        '''
         # Get possible next positions
         next_positions = get_next_pos(current_state.board, cur_pos)
         
         for next_pos in next_positions:
-            # Move to the new position and get the new board state
+            ''' MAKE NEW BOARD '''
             new_board = move(current_state.board, next_pos, cur_pos, list_check_point)
+            ''' IF THIS BOARD DON'T HAVE IN LIST BEFORE --> SKIP THE STATE '''
+            if is_board_exist(new_board, list_state):
+                continue
+            ''' IF ONE OR MORE BOXES ARE STUCK IN THE CORNER --> SKIP THE STATE '''
+            if is_board_can_not_win(new_board, list_check_point):
+                continue
+            ''' IF ALL BOXES ARE STUCK --> SKIP THE STATE '''
+            if is_all_boxes_stuck(new_board, list_check_point):
+                continue
+
+            ''' MAKE NEW STATE '''
+            new_state = state(new_board, current_state, list_check_point)
+            ''' CHECK WHETHER THE NEW STATE IS GOAL OR NOT '''
+            if check_win(new_board, list_check_point):
+                print("Found win")
+                return (new_state.get_line(), len(list_state))
             
-            if not is_board_exist(new_board, visited):
-                new_state = state(new_board, current_state, list_check_point)
-                stack.append(new_state)  # Add new state to the stack
-    
-    return None  
+            ''' APPEND NEW STATE TO PRIORITY QUEUE AND TRAVERSED LIST '''
+            list_state.append(new_state)
+            stack.append(new_state)  # Add new state to the stack
+
+            ''' COMPUTE THE TIMEOUT '''
+            end_time = time.time()
+            if end_time - start_time > TIME_OUT:
+                return []
+        end_time = time.time()
+        if end_time - start_time > TIME_OUT:
+            return []
+    print("Not Found")
+    return []
--- ucs.py
+++ ucs.py
@@ -1,8 +1,19 @@
 from queue import PriorityQueue
 from support_function import*
+import time
 
+
+'''
+//========================//
+//          UCS           //
+//        ALGORITHM       //
+//     IMPLEMENTATION     //
+//========================//
+'''
 def USC_search(initial_board, list_check_point):
     """Perform Uniform Cost Search to find the solution."""
+    start_time = time.time()
+    
     pq = PriorityQueue()
     
     start_state = state(initial_board, None, list_check_point)
@@ -24,17 +35,34 @@ def USC_search(initial_board, list_check_point):
         
         # Find the player's current position
         cur_pos = find_position_player(current_state.board)
-        
+        ''' 
+        THIS WILL PRINT THE STEP-BY-STEP IMPLEMENTATION OF HOW THE ALGORITHM WORKS, 
+        UNCOMMENT TO USE IF NECCESSARY 
+        '''
+        '''
+        time.sleep(1)
+        clear = lambda: os.system('cls')
+        clear()
+        print_matrix(now_state.board)
+        print("State visited : {}".format(len(list_state)))
+        print("State in queue : {}".format(len(list_visit)))
+        '''
         # Get possible next positions
         next_positions = get_next_pos(current_state.board, cur_pos)
         
         for next_pos in next_positions:
-            # Move to the new position and get the new board state
+            # Di chuyển đến vị trí mới và lấy trạng thái bảng mới
             new_board = move(current_state.board, next_pos, cur_pos, list_check_point)
             
             if not is_board_exist(new_board, visited):
-                new_cost = current_cost + 1  # Increment the cost
+                new_cost = current_cost + 1  # Tăng chi phí
                 new_state = state(new_board, current_state, list_check_point)
                 pq.put((new_cost, new_state))
-    
-    return None  
+            end_time = time.time()
+            if end_time - start_time > TIME_OUT:
+                return []
+        end_time = time.time()
+        if end_time - start_time > TIME_OUT:
+            return []
+    print("Not Found")
+    return []
